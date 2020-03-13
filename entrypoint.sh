80f446406dc811c2e0db963fd740d9f2d983c7aa
ruanchaves <ruanchaves@gmail.com>
2020-03-13 20:22:47 -0300

    first version

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,1 @@
-PEOPLEcut="./PEOPLEdata"
-BERT_CHINESE_DIR="chinese_L-12_H-768_A-12"
 python run_cut.py --task_name="people" --do_train=True --do_predict=True --data_dir=$PEOPLEcut --vocab_file=$BERT_CHINESE_DIR/vocab.txt --bert_config_file=$BERT_CHINESE_DIR/bert_config.json --init_checkpoint=$BERT_CHINESE_DIR/bert_model.ckpt --max_seq_length=128 --train_batch_size=32 --learning_rate=2e-5 --num_train_epochs=3.0 --output_dir=./output/result_cut/
