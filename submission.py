83dbc99c31535bb3393bd3163dda36d4b6d4f0c9
ruanchaves <ruanchaves93@gmail.com>
2020-03-24 11:06:39 -0300

    submission

--- submission.py
+++ submission.py
@@ -11,10 +11,12 @@ if __name__ == '__main__':
     results = pd.read_csv(options['results'])
     results = results[['token', 'des_seg']]
     results = results.rename(columns={'token': 'joined_ngram', 'des_seg': 'prediction'})
+    results = results.dropna()
+    results = results.drop_duplicates(subset='joined_ngram')
+    
     processed = read_dataframe(options['processed'], field1='joined_ngram', field2='labels')
-    original = pd.read_csv(options['original'])
 
+    original = pd.read_csv(options['original'])
     original = pd.merge(original, processed, on='joined_ngram', how='left')
     original = pd.merge(original, results, on='joined_ngram', how='left')
-    original = original.drop_duplicates(on='joined_ngram')
     original.to_csv("results_enwiki_2019_11_30.tsv", sep='\t')
